56263fa85af819411aa1a128eaed73be9b8d3cdc
Martin Eckardt <m.eckardt@outlook.com>
2025-09-13 14:06:16 +0200

    Specific naming for promises

--- hooks/useConsoleNotifications.ts
+++ hooks/useConsoleNotifications.ts
@@ -1,19 +1,19 @@
 import { toast } from 'sonner';
 
-const useConsoleNotifications = () => {
+// TO-DO move somewhere better
+const getPChainTxExplorerURL = (txID: string, isTestnet: boolean) => {
+    return `https://${isTestnet ? "subnets-test" : "subnets"}.avax.network/p-chain/tx/${txID}`;
+}
 
-    // TO-DO move somewhere better
-    const getPChainTxExplorerURL = (txID: string, isTestnet: boolean) => {
-        return `https://${isTestnet ? "subnets-test" : "subnets"}.avax.network/p-chain/tx/${txID}`;
-    }
+const useConsoleNotifications = () => {
 
     // TO-DO this should not be a thing. Core wallet should not be possible to be not set
     const sendCoreWalletNotSetNotification = () => {
         toast.error('Core wallet not set');
     }
 
-    const sendCreateSubnetNotifications = (promise: Promise<string>, isTestnet: boolean) => {
-        toast.promise(promise, {
+    const sendCreateSubnetNotifications = (createSubnetTx: Promise<string>, isTestnet: boolean) => {
+        toast.promise(createSubnetTx, {
             loading: 'Creating Subnet...',
             success: (txID: string) => ({
                 message: 'Subnet created successfully',
@@ -25,7 +25,7 @@ const useConsoleNotifications = () => {
             error: (error) => 'Failed to create Subnet: ' + error.message,
         });
 
-        promise
+        createSubnetTx
             .then((txID) => {
                 // TO-DO Add to History
             })
@@ -34,8 +34,8 @@ const useConsoleNotifications = () => {
             });
     };
 
-    const sendCreateChainNotifications = (promise: Promise<string>, isTestnet: boolean) => {
-        toast.promise(promise, {
+    const sendCreateChainNotifications = (createChainTx: Promise<string>, isTestnet: boolean) => {
+        toast.promise(createChainTx, {
             loading: 'Creating Chain...',
             success: (txID: string) => ({
                 message: 'Chain created successfully',
@@ -47,7 +47,7 @@ const useConsoleNotifications = () => {
             error: (error) => 'Failed to create Chain: ' + error.message,
         });
 
-        promise
+        createChainTx
             .then((txID) => {
                 // TO-DO Add to History
             })
