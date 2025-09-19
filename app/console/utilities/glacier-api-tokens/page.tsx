2f5b74227130ecf1a9c5d3296e92b21582d8feec
containerman17 <8990432+containerman17@users.noreply.github.com>
2025-09-19 07:04:53 +0000

    asymmetric encryption

--- app/console/utilities/glacier-api-tokens/page.tsx
+++ app/console/utilities/glacier-api-tokens/page.tsx
@@ -1,36 +1,22 @@
 import TokenManagement from "@/components/toolbox/console/utilities/glacier-api-tokens/TokenManagement";
 import { getAuthSession } from "@/lib/auth/authSession";
 import { redirect } from "next/navigation";
-import jwt from "jsonwebtoken";
-
-// TODO: replace with proper secret management
+import { createGlacierJWT } from "@/lib/glacier-jwt";
 
 export default async function Page() {
   const session = await getAuthSession();
 
-  if (!process.env.DATA_API_JWT_SECRET) {
-    throw new Error("DATA_API_JWT_SECRET is not set");
-  }
-
-  const JWT_SECRET = process.env.DATA_API_JWT_SECRET as string;
-  // If not authenticated , redirect to login
+  // If not authenticated, redirect to login
   if (!session) {
     redirect("/login?callbackUrl=/console/utilities/glacier-api-tokens");
   }
 
-  // Generate JWT for Glacier API
-  const glacierJwt = jwt.sign(
-    {
-      sub: session.user.id,
-      iss: "https://build.avax.network/",
-      email: session.user.email,
-    },
-    JWT_SECRET,
-    {
-      expiresIn: '1h', // 60 minutes
-      algorithm: 'HS256'
-    }
-  );
+  // Generate asymmetric JWT for Glacier API
+  const glacierJwt = await createGlacierJWT({
+    sub: session.user.id,
+    iss: "https://build.avax.network/",
+    email: session.user.email!,
+  });
 
   // Pass authenticated user data to the component
   return (
