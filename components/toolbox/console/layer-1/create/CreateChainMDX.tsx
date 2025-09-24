b18691be206c66846f34daa46bd948db01bd71b7
Owen <owenwahlgren@gmail.com>
2025-09-24 14:05:41 -0400

    fix missing wallet req

--- components/toolbox/console/layer-1/create/CreateChainMDX.tsx
+++ components/toolbox/console/layer-1/create/CreateChainMDX.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { CreateChain } from "./CreateChain";
+import { CheckWalletRequirements } from "@/components/toolbox/components/CheckWalletRequirements";
+import { WalletRequirementsConfigKey } from "@/components/toolbox/hooks/useWalletRequirements";
 
 /**
  * Wrapper component for CreateChain to be used in MDX files.
  * Sets the embedded prop to true to use single-column layout.
+ * Wraps with CheckWalletRequirements to provide the ConnectedWalletProvider.
  */
 export default function CreateChainMDX() {
-    return <CreateChain embedded={true} />;
+    return (
+        <CheckWalletRequirements configKey={WalletRequirementsConfigKey.PChainBalance}>
+            <CreateChain embedded={true} />
+        </CheckWalletRequirements>
+    );
 }
