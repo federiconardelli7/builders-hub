8031ec44c0d845606b14d173cd1f9b113497c6b9
containerman17 <8990432+containerman17@users.noreply.github.com>
2025-10-06 06:19:24 +0000

    fix: update managed testnet nodes service URL to support environment overrides

--- app/api/managed-testnet-nodes/constants.ts
+++ app/api/managed-testnet-nodes/constants.ts
@@ -1,6 +1,12 @@
 
-export const MANAGED_TESTNET_NODES_SERVICE_URL = 'https://multinode-experimental.solokhin.com';
+export let MANAGED_TESTNET_NODES_SERVICE_URL = process.env.MANAGED_NODES_OVERRIDE ||
+  (process.env.VERCEL_ENV === "production"
+    ? 'https://nodes-prod.18.182.4.86.sslip.io'
+    : 'https://nodes-staging.35.74.237.34.sslip.io');
 
+if (MANAGED_TESTNET_NODES_SERVICE_URL.endsWith('/')) {
+  MANAGED_TESTNET_NODES_SERVICE_URL = MANAGED_TESTNET_NODES_SERVICE_URL.slice(0, -1);
+}
 
 // Managed Testnet Nodes service endpoints
 export const ManagedTestnetNodesServiceURLs = {
