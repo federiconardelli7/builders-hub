1125865c1303abab2e187bb2fd9b810a10a67f29
Owen <owenwahlgren@gmail.com>
2025-10-26 20:25:57 -0400

    add read PoAManager step

--- app/console/permissioned-l1s/multisig-setup/steps.ts
+++ app/console/permissioned-l1s/multisig-setup/steps.ts
@@ -2,9 +2,11 @@ import { type StepDefinition } from "@/components/console/step-flow";
 import DeployPoAManager from "@/components/toolbox/console/permissioned-l1s/multisig-setup/DeployPoAManager";
 import TransferOwnership from "@/components/toolbox/console/permissioned-l1s/multisig-setup/TransferOwnership";
 import ReadContract from "@/components/toolbox/console/permissioned-l1s/validator-manager-setup/ReadContract";
+import ReadPoAManager from "@/components/toolbox/console/permissioned-l1s/multisig-setup/ReadPoAManager";
 
 export const steps: StepDefinition[] = [
     { type: "single", key: "deploy-poa-manager", title: "Deploy POA Manager", component: DeployPoAManager },
     { type: "single", key: "transfer-ownership", title: "Transfer Ownership", component: TransferOwnership },
-    { type: "single", key: "read-contract", title: "Read Contract", component: ReadContract },
+    { type: "single", key: "read-poa-manager", title: "Read PoA Manager", component: ReadPoAManager },
+    { type: "single", key: "read-validator-manager", title: "Read Validator Manager", component: ReadContract },
 ];
